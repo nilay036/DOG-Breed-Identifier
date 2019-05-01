ddb4a387ec89c5cb1af1d13b38aa050defd4e1a7
Jackson Bellinger <jdunnbellinger@gmail.com>
2019-05-01 09:49:51 -0400

    added dog images and a data training/testing splitting function

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import random
 import keras
 from keras.models import Sequential
 from keras.layers import Conv2D
@@ -11,24 +13,62 @@ from keras import backend
 from keras.models import Sequential
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import Sequential
-from keras.preprocessing.image import ImageDataGenerator
+#from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import backend
 import cv2
-import numpy as np 
+import numpy as np
 
 from keras.preprocessing.image import ImageDataGenerator
-im_width,im_height=700,700
-m=Sequential()
+# im_width,im_height=700,700
+# m=Sequential()
 
+def splitData(dir, ratio):
+	#dir is the name of the directory, can be relative
+	#ratio is the ratio of training data to test data 0 < ratio < 1
+
+	classes = [name for name in os.listdir(dir)
+				if os.path.isdir(os.path.join('.', dir, name))]
+	print(classes)
+	n = len(classes)
+	for imgclass in classes:
+		classDir = os.path.join('.', dir, imgclass)
+		data = [name for name in os.listdir(classDir)
+				if os.path.isfile(os.path.join(classDir, name))]
+
+		# os.mkdir(os.path.join('.', dir, "train"))
+		# os.mkdir(os.path.join('.', dir, "test"))
+		train_dir = os.path.join('.', dir, "train", imgclass)
+		test_dir = os.path.join('.', dir, "test", imgclass)
+		if not os.path.exists(train_dir):
+			os.makedirs(train_dir)
+		if not os.path.exists(test_dir):
+			os.makedirs(test_dir)
+		ravg = 0
+		for d in data:
+			src = os.path.join(classDir, d)
+			if random.random() <= ratio:
+				dst = os.path.join('.', dir, "train", imgclass, d)
+				ravg+=1
+			else:
+				dst = os.path.join('.', dir, "test", imgclass)
+			print("src", os.path.exists(src ) )
+			print("dst", os.path.exists(dst ) )
+			shutil.move(src, dst)
+		ravg /= n
+		print(str(ravg))
+
+
+splitData('Images', .8)
+#%%
 def load_data(img_dir):
 	#train_images_paths = [os.path.join(img_dir,'train/',img_path) for img_path in os.listdir(os.path.join(img_dir,'train/'))]
-	#test_images_paths = [os.path.join(img_dir,'test/',img_path) for img_path in os.listdir(os.path.join(img_dir,'test/'))]	
-	
+	#test_images_paths = [os.path.join(img_dir,'test/',img_path) for img_path in os.listdir(os.path.join(img_dir,'test/'))]
+
 	train_datagen = ImageDataGenerator(
-            rescale=2./255,
-            horizontal_flip=True,
-            vertical_flip=True
-        )
+			rescale=2./255,
+			horizontal_flip=True,
+			vertical_flip=True
+		)
 
 	test_datagen = ImageDataGenerator(rescale=1./255)
 
@@ -42,10 +82,10 @@ def load_data(img_dir):
 			os.path.join(img_dir,'test/'),
 			target_size=(im_width,im_height),
 			batch_size=20,
-            class_mode='binary')
+			class_mode='binary')
 	return train_generator,validation_generator
 
-    
+
 
 def build_model():
 	m.add(Conv2D(64,(3,3), input_shape=(im_width,im_height,3), activation='sigmoid'))
@@ -56,49 +96,48 @@ def build_model():
 	m.add(Dense(32,activation='sigmoid'))
 	m.add(Dense(1,activation='sigmoid'))
 	m.compile(
-        optimizer='adam',
-        metrics=['accuracy'],
-        loss='binary_crossentropy')
-	    
+		optimizer='adam',
+		metrics=['accuracy'],
+		loss='binary_crossentropy')
+
 
 
 def train_model(train_data,test_data):
 	m.fit_generator(
 		train_data,
-        steps_per_epoch=40,
-        epochs=5,
-        validation_data=test_data,
-        validation_steps=40,
-        use_multiprocessing=False,
-        workers=4
-        
-        )
+		steps_per_epoch=40,
+		epochs=5,
+		validation_data=test_data,
+		validation_steps=40,
+		use_multiprocessing=False,
+		workers=4
+
+		)
 def invert_mapping(d):
 	inverted=dict()
-    
+
 	for key , value in d.items():
 		inverted[value]=key
-        
+
 	return inverted
 
 def predict(img_dir):
 	img_paths = [os.path.join(img_dir,'predict/',img_path) for img_path in os.listdir(os.path.join(img_dir,'predict/'))]
 	images=[cv2.imread(img) for img in img_paths]
-	images=[cv2.resize(img,(im_height,im_width)) for img in images]  
-	images=[np.reshape(img,[1,im_height,im_width,3]) for img in images]    
-	return [(m.predict_classes(img)[0][0], img_paths[i]) for i, img in enumerate(images)]   
-    
-    
+	images=[cv2.resize(img,(im_height,im_width)) for img in images]
+	images=[np.reshape(img,[1,im_height,im_width,3]) for img in images]
+	return [(m.predict_classes(img)[0][0], img_paths[i]) for i, img in enumerate(images)]
+
+
 try:
 	if __name__=='__main__':
 		train_data,test_data=load_data('img')
 		build_model()
 		train_model(train_data,test_data)
-	predictions=predict('img') 
+	predictions=predict('img')
 	mapping=invert_mapping(train_data.class_indices)
 	for val, im_name in predictions:
 		print(f'We think that {im_name} is {mapping[val]}')
 	print(predictions)
 except KeyboardInterrupt:
 	print('\nUser aborted!')
- 
